b61518b444c16623b8fa70c6df06ef35c97cbb7b
Hrishikesh Rana <itsranahrishikesh@gmail.com>
2025-07-16 22:26:52 +0530

    changed to testnet

--- src/ai/tools/sui/utils.ts
+++ src/ai/tools/sui/utils.ts
@@ -1,5 +1,5 @@
 import { SuiClient } from '@mysten/sui/client';
 
 export const Testnetclient = new SuiClient({
-  url: process.env.PUBLIC_NODE || 'https://fullnode.mainnet.sui.io',
+  url: process.env.PUBLIC_NODE || 'https://fullnode.testnet.sui.io',
 });
